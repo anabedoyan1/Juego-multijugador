4d960bcf6898a08b1bf16d48e6e33bdeb4e7437d
DanielSerna31 <danisernangel98@gmail.com>
2020-11-02 00:13:03 -0500

    Added name to tanks

---
+++ photon-rooms-and-lobbys-master/Assets/Scripts/WorldToHUDSprite.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class WorldToHUDSprite : MonoBehaviour
+{
+    // Update is called once per frame
+    protected virtual void LateUpdate()
+    {
+        if (Camera.main != null) {
+            this.transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+}
